0d0399e25605e5372276615bb18982480c0dbe9f
Mehdih77 <mehdiwork77@gmail.com>
2021-09-19 12:30:17 +0430

    working on aboutUs page and finish top part styles

--- src/Pages/AboutUsPage/AboutUsPage.jsx
+++ src/Pages/AboutUsPage/AboutUsPage.jsx
@@ -5,18 +5,43 @@ import '../../Style/main-style.css';
 export default function AboutUsPage() {
     return (
         <>
-            <section>
-                <div className='container my-4'>
-                    <div className='row contact-custom-style'>
-                    <h5 className='contact-title'> بـــا مـــا همراه بـــــــــــــــــــاشید</h5>
-                   <div className='contact'>
-                   <ul className='contact-social'>
-                        <li> <a rel="noreferrer" href='https://www.instagram.com/frontend.plus/' target='_blank'><i className="fab fa-instagram"></i> </a> </li>
-                        <li> <a href='#!'> <i className="fab fa-telegram-plane"></i></a> </li>
-                        <li> <a href='#!'> <i className="fab fa-linkedin"></i></a> </li>
-                        <li> <a href='#!'> <i className="fab fa-youtube"></i></a> </li>
-                    </ul>
+            <section className='aboutUs'>
+                <div className='container-fluid aboutUs-header my-4'>
+                    <div className="container">
+                        <div className="row">
+                            <div className="aboutUs-header-title col-md-5">
+                                <p>آنلاین شاپ</p>
+                                <p>اولین مقصد خرید آنلاین در ایران</p>
+                                <p>می‌خواستیم یک بازار مدرن بر پایه تکنولوژی بسازیم که اولین مرجع آنلاین و مورداعتماد «شما» باشد.</p>
+                            </div>
+                            <div className="col-md-7">
+                                <img className='img-fluid' src="image/aboutus-1.png" alt="aboutsus" />
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div className='container aboutUs-business my-2'>
+                    <p className='aboutUs-business-top-title'>حرفی برای گفتن در تجارت الکترونیک</p>
+                    <p className='aboutUs-business-bottom-title'>در سال ۱۳۸۵ با سرمایه اندکی شروع کردیم و حالا این عددها، سهم بزرگ ما از بازار تجارت الکترونیک را نشان می‌دهد.</p>
+                    <div className='aboutUs-business-topics pb-3'>
+                        <div className='aboutUs-business-topics-single'>
+                            <i className="fas fa-store"></i>
+                            <p>۱۵۰،۰۰۰+</p>
+                            <p>کسب‌و‌کار و فروشنده</p>
+                            <a href="#!">فروشنده شوید <span>{`>`}</span></a>
+                        </div>
+                        <div className='aboutUs-business-topics-single'>
+                            <i className="fas fa-clipboard-check"></i>
+                            <p>۴،۵۰۰،۰۰۰+</p>
+                            <p>تنوع کالایی</p>
+                            <a href="#!"> کالاها را ببینید <span>{`>`}</span></a>
+                        </div>
+                        <div className='aboutUs-business-topics-single'>
+                            <i className="fas fa-users"></i>
+                            <p>۳۰،۰۰۰،۰۰۰+</p>
+                            <p>بازدید یکتای ماهانه</p>
+                            <a href="#!"> عضو دیجی کالا شوید <span>{`>`}</span></a>
+                        </div>
                     </div>
                 </div>
             </section>
