24b97221244f3b81297f9e6ff081845fbf7d3e85
Mehdih77 <mehdiwork77@gmail.com>
2021-09-22 20:16:28 +0330

    Finish off-order Sub-menu style (Navbar.jsx)

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -14,6 +14,7 @@ export default function Navbar() {
   const history = useHistory()
   const {curretnUser,logOut} = useAuth();
 
+  // logout
   async function hanldeLogOut(e) {
     e.preventDefault();
     try {
@@ -29,7 +30,7 @@ export default function Navbar() {
   }, [currentProducts,dispatch])
 
   const [openSubMenu, setOpenSubMenu] = useState(false)
-  const handleOpenCloseSubMenu = (e) => {
+  const handleOpenCloseSubMenu = () => {
     setOpenSubMenu(!openSubMenu);
   }
 
@@ -41,9 +42,8 @@ export default function Navbar() {
           aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
          </button>
-
       <nav className="collapse navbar-collapse caustom-navbar" id="navbarSupportedContent">
-      <div className="navbar-top">
+        <div className="navbar-top">
         <Link to='/' className="navbar-logo d-none d-md-block"><img src="/image/home.svg" alt="logo" /></Link>
         <div className='navbar-search'>
           <Search />
@@ -94,14 +94,11 @@ export default function Navbar() {
         </div>
       </div>
         <ul className="navbar-nav navbar-bottom mr-2">
-            <li className="nav-item">
-              <a className="nav-link" href="#!">سوپر مارکت </a>
-            </li>
-            <li className="nav-item dropdown">
+          <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#!" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                تخفیف ها و پیشنهادات
+              <i className="fas fa-bars"></i>دسته بندی کالاها
               </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <div className="dropdown-menu all-goods-submenu container" aria-labelledby="navbarDropdown">
                 <a className="dropdown-item" href="#!">لوازم الکترونیکی</a>
                 <a className="dropdown-item" href="#!"> لوازم خانگی</a>
                 <a className="dropdown-item" href="#!"> لوازم آرایشی</a>
@@ -110,14 +107,65 @@ export default function Navbar() {
                 <a className="dropdown-item" href="#!">دیگر محصولات</a>
               </div>
             </li>
-            <li className="nav-item">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#!" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i className="fas fa-percentage"></i>تخفیف ها و پیشنهادها
+              </a>
+              <div className="dropdown-menu off-order-submenu container" aria-labelledby="navbarDropdown">
+                <div className="row">
+                <div className="col-md-4 off-order-submenu-right">
+                <a href="#!">مشاهده همه تخفیف ها و پیشنهادها<i className="fas fa-angle-left"></i></a>
+                  <a href="#!">کالاهای شگفت انگیز</a>
+                  <a href="#!">شگفت انگیز سوپرمارکتی</a>
+                  <a href="#!">فروش ویژه</a>
+                  <a className='d-none d-md-block' href="#!">کالای دیجیتال</a>
+                  <a className='d-none d-md-block' href="#!">خودرو، ابزار و تجهیزات صنعتی</a>
+                  <a className='d-none d-md-block' href="#!">مد و پوشاک</a>
+                  <a className='d-none d-md-block' href="#!">اسباب بازی،کودک و نوزاد</a>
+                  <a className='d-none d-md-block' href="#!">کالاهای سوپرمارکتی</a>
+                  <a className='d-none d-md-block' href="#!">زیبایی و سلامت</a>
+                  <a className='d-none d-md-block' href="#!">خانه و آشپزخانه</a>
+                  <a className='d-none d-md-block' href="#!">کتاب، لوازم التحریر و هنر</a>
+                  <a className='d-none d-md-block' href="#!">ورزش و سفر</a>
+                </div>
+                <div className="col-md-4 off-order-submenu-middle">
+                  <a className='off-order-submenu-middle-traditional' href="#!">محصولات بومی و محلی</a>
+                  <a className='d-none d-md-block' href="#!"><i className="fas fa-circle"></i>حشره کش و سوسک کش تا 70درصد تخفیف</a>
+                  <a className='d-none d-md-block' href="#!"><i className="fas fa-circle"></i>عطر کمتر از 99هزار تومان</a>
+                  <hr/>
+                  <div>
+                  <a href="#!"><i className="fas fa-fire"></i>پرفروش ترین کالاها</a>
+                  <a href="#!"><i className="fas fa-gift"></i>با هر خرید هدیه بگیرید!</a>
+                  <a href="#!"><i className="fas fa-tag"></i>تخفیف پایان فصل</a>
+                  <a href="#!"><i className="far fa-credit-card"></i>کارت هدیه خرید از دیجی کالا</a>
+                  <a href="#!"><i className="fas fa-gifts"></i>تازه های فروشنده جدید</a>
+                  </div>
+                </div>
+                <div className="col-md-4 off-order-submenu-left">
+                  <img className='img-fluid d-none d-md-block' src="/image/off-order-submenu-img.png" alt="off-order-submenu-img" />
+                </div>
+                </div>
+              </div>
+            </li>
+          <li className="nav-item">
               <Link to='/contact' className="nav-link" href="#">تماس با ما</Link>
             </li>
-            <li className="nav-item">
+          <li className="nav-item">
               <Link to='/aboutus' className="nav-link " href="#" tabIndex="-1">درباره ما</Link>
             </li>
-          </ul>
-        
+          <li className="nav-item">
+              <Link to='/' className="nav-link " href="#" tabIndex="-1"><i className="far fa-check-circle"></i>دیجی کالای من</Link>
+            </li>
+          <li className="nav-item">
+              <Link to='/' className="nav-link " href="#" tabIndex="-1"><i className="fas fa-plus-circle"></i>دیجی پلاس</Link>
+            </li>
+          <li className="nav-item">
+              <Link to='/contact' className="nav-link " href="#" tabIndex="-1">سوالی دارید؟</Link>
+            </li>
+          <li className="nav-item">
+              <Link to='/' className="nav-link " href="#" tabIndex="-1">فروشنده شوید</Link>
+            </li>
+        </ul> 
       </nav>
     </>
     )
